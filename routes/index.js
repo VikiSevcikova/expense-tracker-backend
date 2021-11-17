e133e6aaa4424a280d4cecd2b53d0a33b4721e76
kit486759 <kit486759@gmail.com>
2021-11-16 16:46:25 -0800

    dashboard route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Transaction = require("../models/Transaction");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('<h1>This is the index page.</h1>');
+  Transaction.find()
+  .then(data => res.json(data))
+  .catch(err => res.status(400).json(`Error:${err}`));
+  // res.send('<h1>This is the index page.</h1>');
 });
 
 module.exports = router;
