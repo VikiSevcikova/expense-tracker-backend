5d9c8803725d2bf9d47f62ec11470348343e70f9
kit486759 <kit486759@gmail.com>
2021-11-18 23:51:19 -0800

    req

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,10 +1,40 @@
 const Transaction = require("../models/Transaction");
+const express = require('express')
+const app = express()
+app.use(express.json());
+
+exports.getAllTransaction = (req, res, next) => {
+  Transaction.find()
+    .sort({ date: 1 })
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json(`Error:${err}`));
+};
 
 exports.getRecentTransaction = (req, res, next) => {
-    // get limit 5 items for recent transactions
+  // get limit 5 items for recent transactions
   Transaction.find()
-  .sort({'date': -1})
-  .limit(5)
+    .sort({ date: -1 })
+    .limit(5)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json(`Error:${err}`));
+};
+
+exports.getTranscationByDate = (req, res, next) => {
+ 
+    console.log(req.params)
+    const startDate = req.body.startdate
+    const endDate = req.body.enddate
+  Transaction.find({
+    date: {
+        // gte = Greater Than of Equal
+        // 2021-11-15
+      $gte: startDate,
+       // lte = Lesser Than of Equal
+       // 2021-11-17
+      $lte: endDate,
+    },
+  })
+    .sort({ date: 1 })
     .then((data) => res.json(data))
     .catch((err) => res.status(400).json(`Error:${err}`));
 };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const { getRecentTransaction } = require("../controllers/dashboard");
+const { getAllTransaction, getRecentTransaction, getTranscationByDate } = require("../controllers/dashboard");
 
 router.use(cors());
 
 /* GET home page. */
-router.get("/", getRecentTransaction);
+
+router.get("/all-transaction", getAllTransaction);
+router.get("/recent-transaction", getRecentTransaction);
+router.get("/transaction", getTranscationByDate);
 
 module.exports = router;
