90d2eac4354b154f05b3651a7583a7a81ca7a5c1
Yuki <yukipooh4120@gmail.com>
2021-11-18 21:08:06 -0800

    edit and delete completed

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,6 @@ exports.protect = async (req, res, next) => {
 
     if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
         token = req.headers.authorization.split(" ")[1];
-        // console.log("TOKEN:",token);
     }
 
     if(!token){
