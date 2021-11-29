ace4e2da585430b628a192c65a8815a753677f2d
Yuki <yukipooh4120@gmail.com>
2021-11-29 15:04:41 -0800

    working on edit users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const { currentUser, deleteUser } = require('../controllers/users');
+const { currentUser, deleteUser, editUser } = require('../controllers/users');
 
 /* GET users listing. */
 router.route("/me").get(protect, currentUser);
 router.route("/delete").delete(protect, deleteUser);
+router.route("/edit").delete(protect, editUser);
 
 module.exports = router;
